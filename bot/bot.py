6fd9f3f18bfa2ec678da9070aee2186c92bea922
Murat Dibirov <dibirov.murat@mail.ru>
2025-06-26 16:17:05 +0300

    подготовил папку и все необходимые файлы. обновил requirements

---
+++ bot/bot.py
@@ -0,0 +1,12 @@
+from aiogram import Bot, Dispatcher, executor, types
+from bot.config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    await message.answer("Привет! Я Avito_parser_new_bot.\nНапиши /search чтобы найти объявления.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
