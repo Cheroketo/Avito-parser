c1e891bbff0b74f668c856b631c045bc521a4b9c
Murat Dibirov <dibirov.murat@mail.ru>
2025-06-26 17:28:24 +0300

    есть ответ на start от бота

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -6,7 +6,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    await message.answer("Привет! Я Avito_parser_new_bot.\nНапиши /search чтобы найти объявления.")
+    await message.answer("Привет! Я Avito_parser_new_bot. Напиши /search чтобы найти объявления.")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
